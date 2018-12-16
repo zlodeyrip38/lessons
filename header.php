cfd3f26521d09a36f1fe10f496e1a8f0d8badd16
ilya682003 <ilyanfs682003@mail.ru>
2018-12-16 13:10:03 +0300

    Основная структура сайта

--- header.php
+++ header.php
@@ -8,6 +8,7 @@ require_once "lib/functions.php";
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
     <link href="style/style.css" rel="stylesheet" />
     <title>Document</title>
 </head>
