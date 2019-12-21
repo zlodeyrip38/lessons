e59eeeb94e39a1e94a9bb1ec219762f273307bc8
lyubomyr83 <lyubomyr83@mail.ru>
2019-12-21 16:56:37 +0300

    admin

--- admin/views/VPageAdd.php
+++ admin/views/VPageAdd.php
@@ -1,9 +1,43 @@
 <?php
 ?>
 <form method="post">
-    Названние страницы в меню
-    <input type="text" name="menu_name">
-    Контент
-    <input type="text" name="content">
-    <input type="submit" value="Сохранить">
+
+
 </form>
+
+<div class="content">
+    <div class="row header">
+        <div class="col-md-12">
+            добавление новой страницы
+        </div>
+    </div>
+    <form method="post">
+        <div class="row">
+            <div class="col-md-2">
+                Название страницы в меню
+            </div>
+            <div class="col-md-10">
+                <input type="text" name="menu_name" value="<?=$page['menu_name']?>">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+                Контент
+            </div>
+            <div class="col-md-10">
+            <textarea name="content">
+                <?=$page['content']?>
+            </textarea>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+
+            </div>
+            <div class="col-md-10">
+                <input type="submit" value="Сохранить">
+            </div>
+        </div>
+    </form>
+
+</div>
--- admin/views/VPageEdit.php
+++ admin/views/VPageEdit.php
@@ -1,12 +1,40 @@
 <?php
 $page = dataForPageEdit($_GET['id']);
-
-echo "Редактирование страницы ".$page['menu_name'];
 ?>
-<form method="post">
-    Названние страницы в меню
-    <input type="text" name="menu_name" value="<?=$page['menu_name']?>">
-    Контент
-    </textarea><name="content" value="<?=$page['content']?>"><textarea
-    <input type="submit" value="Сохранить">
-</form>
+
+<div class="content">
+    <div class="row header">
+        <div class="col-md-12">
+            <?="Редактирование страницы ".$page['menu_name']?>
+        </div>
+    </div>
+    <form method="post">
+        <div class="row">
+            <div class="col-md-2">
+                Название страницы в меню
+            </div>
+            <div class="col-md-10">
+                <input type="text" name="menu_name" value="<?=$page['menu_name']?>">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+                Контент
+            </div>
+            <div class="col-md-10">
+            <textarea name="content">
+                <?=$page['content']?>
+            </textarea>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+
+            </div>
+            <div class="col-md-10">
+                <input type="submit" value="Сохранить">
+            </div>
+        </div>
+    </form>
+
+</div>
